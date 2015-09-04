26e167ca0feab81210da88f64f7eee1e2f376158
Jorge Del Rio <jdelrios@gmail.com>
2015-09-04 18:26:14 -0500

    Create missing directories and remove log file

--- initialize.sh
+++ initialize.sh
@@ -11,6 +11,8 @@ fi
 openssl genrsa -out "$CA.key" $BITS
 openssl req -new -x509 -days $DAYS -key "$CA.key" -out "$CA.crt" -subj "/C=$COUNTRY/ST=$STATE/L=$CITY/CN=$CA_COMMON_NAME/O=$ORGANIZATION/"
 
+> log.txt
+
 echo "01" > "$CA.srl"
 
 > crl/index.txt
